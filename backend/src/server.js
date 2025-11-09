4639b4b9fb8e3e62fb86cd311281272286881e56
YashLagare <yashlagare77@gmail.com>
2025-11-09 22:13:04 +0530

    started here

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
 
+
+
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get('/', (req,res) => {
     res.status(200).json('Hello World!');
 });
 
+app.get('/books', (req,res) => {
+    res.status(200).json('Hello Book!');
+});
+
+//for deployment on sevalla
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 
 app.listen(ENV.PORT, () => {
